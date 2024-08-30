18906df0860985eeb2990030e057de670e393199
Feerdus95 <ferdus95.fv@gmail.com>
2024-08-30 01:05:26 -0300

    Exercises 2.16 - 2.17

--- part2/phonebook/eslint.config.js
+++ part2/phonebook/eslint.config.js
@@ -1,73 +1,39 @@
-import './App.css'
-import { useState } from 'react'
+import js from '@eslint/js'
+import globals from 'globals'
+import react from 'eslint-plugin-react'
+import reactHooks from 'eslint-plugin-react-hooks'
+import reactRefresh from 'eslint-plugin-react-refresh'
 
-const Title = () => <h1>Phonebook / Telefónica</h1>
-const Person = ({ person }) => { return <p>{ person.name } { person.number }</p> }
-const Filter = ({ filter, handleFilter }) => {
-  return <div>Filter shown with <input value={filter} onChange={handleFilter} /></div>
-}
-
-const App = () => {
-  const [persons, setPersons] = useState([
-    { name: 'Arto Hellas', number: '040-123456', id: 1 },
-    { name: 'Ada Lovelace', number: '39-44-5323523', id: 2 },
-    { name: 'Dan Abramov', number: '12-43-234345', id: 3 },
-    { name: 'Mary Poppendieck', number: '39-23-6423122', id: 4 }
-  ])
-  const [newName, setNewName] = useState('')
-  const [newNumber, setNewNumber] = useState('')
-  const [filter, setFilter] = useState('')
-
-  const addName = (event) => {
-    event.preventDefault()
-    const newPerson = { name: newName, number: newNumber, id: persons.length + 1 }
-    setPersons(persons.concat(newPerson))
-    setNewName('')
-    setNewNumber('')
-  }
-
-  const handleNameChange = (event) => {
-    setNewName(event.target.value)
-  }
-
-  const handleNumberChange = (event) => {
-    setNewNumber(event.target.value)
-  }
-
-  const handleFilterChange = (event) => {
-    setFilter(event.target.value)
-  }
-
-  const checkDuplicateName = persons.some(person => person.name === newName)
-  if (checkDuplicateName) {
-    alert(`${newName} has already been added to phonebook`)
-    setNewName('')
-  }
-
-  const personsToShow = persons.filter(person => 
-    person.name.toLowerCase().includes(filter.toLowerCase()) || 
-    person.number.includes(filter)
-  )
-
-  return (
-    <div>
-      <Title />
-      <Filter filter={filter} handleFilter={handleFilterChange} />
-      <form onSubmit={addName} id='add'>
-        <div>
-          Name / Nombre: <input value={newName} onChange={handleNameChange} />
-        </div>
-        <div>
-          Number / Número: <input value={newNumber} onChange={handleNumberChange} />
-        </div>
-        <div>
-          <button type="submit">Add / Añadir</button>
-        </div>
-      </form>
-      <h2>Numbers / Números</h2>
-      {personsToShow.map(person => <Person key={person.id} person={person} />)}
-    </div>
-  )
-}
-
-export default App
+export default [
+  { ignores: ['dist'] },
+  {
+    files: ['**/*.{js,jsx}'],
+    languageOptions: {
+      ecmaVersion: 2020,
+      globals: globals.browser,
+      parserOptions: {
+        ecmaVersion: 'latest',
+        ecmaFeatures: { jsx: true },
+        sourceType: 'module',
+      },
+    },
+    settings: { react: { version: '18.3' } },
+    plugins: {
+      react,
+      'react-hooks': reactHooks,
+      'react-refresh': reactRefresh,
+    },
+    rules: {
+      ...js.configs.recommended.rules,
+      ...react.configs.recommended.rules,
+      ...react.configs['jsx-runtime'].rules,
+      ...reactHooks.configs.recommended.rules,
+      'react/jsx-no-target-blank': 'off',
+      'react-refresh/only-export-components': [
+        'warn',
+        { allowConstantExport: true },
+      ],
+      'react/prop-types': 'off',
+    },
+  },
+]
--- part2/phonebook/src/App.jsx
+++ part2/phonebook/src/App.jsx
@@ -1,12 +1,14 @@
 import './App.css'
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
+import personService from './services/personService'
+import { v4 as uuidv4 } from 'uuid'
 
 const Title = () => <h1>Phonebook / Telefónica</h1>
-const Person = ({ person }) => { return <p>{ person.name } { person.number }</p> }
-const Filter = ({ filter, handleFilter }) => {
-  return <div>Filtered with / Filtrado por <input value={ filter } onChange={ handleFilter } /></div>
-}
+const Notification = ({ message }) => (  message === null ? null : <div className='notification'>{ message }</div> );
+const Person = ({ person, handleDelete }) => { return <p>{ person.name } { person.number } <button onClick={ () => handleDelete(person.id) }>Delete / Eliminar</button></p> }
+const Filter = ({ filter, handleFilter }) => { return <div>Filtered with / Filtrado por <input value={ filter } onChange={ handleFilter } /></div> }
 const PersonForm = ({ addName, newName, handleNameChange, newNumber, handleNumberChange }) => {
+
   return (
     <form onSubmit={ addName }>
       <div>
@@ -21,33 +23,68 @@ const PersonForm = ({ addName, newName, handleNameChange, newNumber, handleNumbe
     </form>
   )
 }
-const Persons = ({ persons, filter }) => {
+const Persons = ({ persons, filter, handleDelete }) => {
   return (
     <div>
       { persons
         .filter( person => person.name.toLowerCase().includes(filter.toLowerCase()))
-        .map( person => <Person key={ person.id } person={ person } />)}
+        .map( person => <Person key={ person.id } person={ person } handleDelete={ handleDelete } />)}
     </div>
   )
 }
 
 const App = () => {
-  const [persons, setPersons] = useState([
-    { name: 'Arto Hellas', number: '040-123456', id: 1 },
-    { name: 'Ada Lovelace', number: '39-44-5323523', id: 2 },
-    { name: 'Dan Abramov', number: '12-43-234345', id: 3 },
-    { name: 'Mary Poppendieck', number: '39-23-6423122', id: 4 }
-  ])
+  const [persons, setPersons] = useState([])
   const [newName, setNewName] = useState('')
   const [newNumber, setNewNumber] = useState('')
   const [filter, setFilter] = useState('')
+  const [message, setMessage] = useState(null)
+
+  useEffect(() => {
+    personService
+      .getAll()
+      .then(initialPersons => {
+        setPersons(initialPersons)
+      })
+  }, [])
 
   const addName = (event) => {
     event.preventDefault()
-    const newPerson = { name: newName, number: newNumber, id: persons.length + 1 }
-    setPersons(persons.concat(newPerson))
-    setNewName('')
-    setNewNumber('')
+    const newPerson = { name: newName, number: newNumber, id: uuidv4() }
+    const existingPerson = persons.find(person => person.name === newName);
+    if (existingPerson) {
+      if (window.confirm(`${newName} is already added to phonebook, replace the old number with a new one?`)) {
+        const updatedPerson = { ...existingPerson, number: newNumber };
+        personService
+          .update(existingPerson.id, updatedPerson)
+          .then(returnedPerson => {
+            setPersons(persons.map(person => person.id !== existingPerson.id ? person : returnedPerson));
+            setNewName('');
+            setNewNumber('');
+            setMessage(`${newName}'s number has been updated. / número ha sido actualizado.`)
+            setTimeout(() => { setMessage(null) }, 5000)
+          })
+          .catch(error => {
+            console.log(error.response.data);
+            alert(error.response.data.error);
+          });
+      }
+      return;
+    }
+
+    personService
+      .create(newPerson)
+      .then(returnedPerson => {
+        setPersons(persons.concat(returnedPerson))
+        setNewName('')
+        setNewNumber('')
+        setMessage(`${newName} has been added to phonebook. / ha sido agregado a la agenda.`)
+        setTimeout(() => { setMessage(null) }, 5000)
+      })
+      .catch(error => {
+        console.log(error.response.data)
+        alert(error.response.data.error)
+      })
   }
 
   const handleNameChange = (event) => {
@@ -60,11 +97,23 @@ const App = () => {
     setFilter(event.target.value)
   }
 
-  const checkDuplicateName = persons.some(person => person.name === newName)
-  if (checkDuplicateName) {
-    alert(`${newName} already on the list / ya en la lista`)
-    setNewName('')
-  }
+  const handleDelete = (id) => {
+    const person = persons.find(person => person.id === id);
+    if (window.confirm(`Delete ${person.name}?`)) {
+      personService
+        .remove(id)
+        .then(() => {
+          setPersons(persons.filter(person => person.id !== id));
+          setMessage(`${person.name} has been deleted from phonebook. / ha sido eliminado de la agenda.`)
+          setTimeout(() => { setMessage(null) }, 5000)
+        })
+        .catch(error => {
+          console.log(error.response.data);
+          alert(error.response.data.error);
+        });
+    }
+  };
+
   const personsList = persons.filter(person => 
     person.name.toLowerCase().includes(filter.toLowerCase()) || 
     person.number.includes(filter)
@@ -73,9 +122,10 @@ const App = () => {
   return (
     <div>
       <Title />
+      <Notification message={ message } />
       <Filter filter={ filter } handleFilter={ handleFilterChange } />
       <PersonForm addName={ addName } newName={ newName } handleNameChange={ handleNameChange } newNumber={ newNumber } handleNumberChange={ handleNumberChange } />
-      <Persons persons={ personsList } filter={ filter } />
+      <Persons persons={ personsList } filter={ filter } handleDelete={ handleDelete } />
     </div>
   )
 }
